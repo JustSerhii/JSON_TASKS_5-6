7fd39c0bf74c0a4f3fe1a221a2fccff0ac29eb20
Serhii <scimitarnew@gmail.com>
2023-02-27 18:50:23 +0200

    commit 0

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            River river1 = new River("Buzh");
+            river1.GetInfo();
+
+
+            Console.WriteLine("\n\n");
+
+
+            river1.PrintDetails();
+
+            Console.WriteLine("\n\n");
+
+            Mountain rock1 = new Mountain("Hoverla");
+            rock1.GetInfo();
+            rock1.PrintDetails();
+
+        }
+    }
+    interface IGeoObject
+    {
+        void GetInfo();
+    }
+
+    abstract class GeoObject : IGeoObject
+    {
+        protected double coX;
+        protected double coY;
+        protected string name;
+        protected string description;
+
+        public GeoObject(string Name)
+        {
+            this.name = Name;
+        }
+
+        public virtual void GetInfo()
+        {
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Name: " + name + "\nX: " + coX + "\nY: " + coY + "\nDescription: " + description);
+        }
+
+        abstract public void PrintDetails();
+    }
+
+    class River : GeoObject
+    {
+
+        protected double flowSpeed;
+        protected double length;
+
+        public River(string Name) : base(Name)
+        {
+            this.name = Name;
+        }
+        public override void GetInfo()
+        {
+            Console.Write("CoX: "); coX = Convert.ToDouble(Console.ReadLine());
+            Console.Write("CoY: "); coY = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Description: "); description = Console.ReadLine();
+            Console.Write("The speed of the flow: "); flowSpeed = Convert.ToDouble(Console.ReadLine());
+            Console.Write("The length of the river: "); length = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public override void PrintDetails()
+        {
+            PrintInfo();
+            Console.WriteLine("Speed of the flow: " + flowSpeed + "\nThe length of the river: " + length);
+        }
+
+    }
+    class Mountain : GeoObject
+    {
+
+        protected double highestPoint;
+
+        public Mountain(string Name) : base(Name)
+        {
+            this.name = Name;
+        }
+        public override void GetInfo()
+        {
+            Console.Write("The highest point: "); highestPoint = Convert.ToDouble(Console.ReadLine());
+            Console.Write("CoX: "); coX = Convert.ToDouble(Console.ReadLine());
+            Console.Write("CoY: "); coY = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Description: "); description = Console.ReadLine();
+        }
+
+        public override void PrintDetails()
+        {
+            PrintInfo();
+            Console.WriteLine("The highest point: " + highestPoint);
+        }
+
+    }
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace JSON_Practice
+{
+    public class PublishingHouse
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public string Adress { get; }
+
+        public PublishingHouse(int id, string name, string adress)
+        {
+            Id = id;
+            Name = name;
+            Adress = adress;
+        }
+    }
+    public class Book
+    {
+        [JsonIgnore]
+        public int PublishingHouseId { get; }
+
+        [Newtonsoft.Json.JsonProperty(PropertyName = "Name")]
+
+        public string Title { get; }
+        public PublishingHouse PublishingHouse { get; }
+
+        public Book(int publishingHouseId, string title, PublishingHouse publishingHouse)
+        {
+            PublishingHouseId = publishingHouseId;
+            PublishingHouse = publishingHouse;
+            Title = title;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\ks\RiderProjects\Solution1\input.json";
+            string text = File.ReadAllText(path);
+
+            var books = JsonSerializer.Deserialize<List<Book>>(text);
+
+            foreach (var book in books)
+            {
+                Console.WriteLine(JsonSerializer.Serialize(book));
+            }
+        }
+    }
+}
